9fa3972458939509401e2d4e71a49242885c04f8
James Wolff <james.wolff0@gmail.com>
2016-04-08 08:38:44 -0500

    Moved logo and prologue to resources folder, added resource text files

--- src/test/java/io/github/jwolff52/cyoa/Driver.java
+++ src/test/java/io/github/jwolff52/cyoa/Driver.java
@@ -2,19 +2,17 @@ package io.github.jwolff52.cyoa;
 
 import io.github.jwolff52.cyoa.res.R;
 
-import java.io.File;
-import java.util.Random;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Driver {
 
-    public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-//            System.out.println(String.format("%d: %d", i, i*(i*30/100) < 100 ? 100 : i*(i*30/100)));
-            System.out.println(String.format("%d: %d", i, 20*(i/2) + 100));
-        }
-
-        for (File f : R.SAVE_HOME.listFiles()) {
-            System.out.println(f.getName());
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(R.getResourceAsStream("/logo.txt")));
+        String line;
+        while((line = reader.readLine()) != null) {
+            System.out.println(line);
         }
     }
 }
