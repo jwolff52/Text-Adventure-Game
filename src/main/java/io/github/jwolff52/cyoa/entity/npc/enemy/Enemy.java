8a1fa0561f3c99c1d3fe483e1818832d50f2d778
Caymon <sulliv62@students.wwu.edu>
2016-05-04 21:10:17 -0700

    boop

--- src/main/java/io/github/jwolff52/cyoa/entity/npc/enemy/Enemy.java
+++ src/main/java/io/github/jwolff52/cyoa/entity/npc/enemy/Enemy.java
@@ -1,11 +1,11 @@
-package io.github.jwolff52.cyoa.entity.npc.monster;
+package io.github.jwolff52.cyoa.entity.npc.enemy;
 
 import io.github.jwolff52.cyoa.entity.npc.NPC;
 
 /**
  * Created by James on 4/7/2016.
  */
-public abstract class Monster extends NPC {
+public abstract class Enemy extends NPC {
     private int level;
 
     public int getLevel() {
