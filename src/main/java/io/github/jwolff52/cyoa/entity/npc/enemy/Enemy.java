6fc2213ca2474e24eca21eb46f9ba861a10bb728
James Wolff <james.wolff0@gmail.com>
2016-07-11 19:47:30 -0500

    Updated Enemy to use the new NPC API

--- src/main/java/io/github/jwolff52/cyoa/entity/npc/enemy/Enemy.java
+++ src/main/java/io/github/jwolff52/cyoa/entity/npc/enemy/Enemy.java
@@ -1,36 +1,19 @@
-<<<<<<< HEAD:src/main/java/io/github/jwolff52/cyoa/entity/npc/monster/Monster.java
-package io.github.jwolff52.cyoa.entity.npc.monster;
-
-import io.github.jwolff52.cyoa.entity.npc.GenericNPC;
-
-/**
- * Created by James on 4/7/2016.
- */
-public abstract class Monster extends GenericNPC {
-    private int level;
-
-    public Monster(String name) {
-        super(name);
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-}
-=======
 package io.github.jwolff52.cyoa.entity.npc.enemy;
 
-import io.github.jwolff52.cyoa.entity.npc.NPC;
+import io.github.jwolff52.cyoa.entity.npc.GenericNPC;
+import io.github.jwolff52.cyoa.entity.npc.NPCType;
+
+import java.util.ArrayList;
 
 /**
  * Created by James on 4/7/2016.
  */
-public abstract class Enemy extends NPC {
+public abstract class Enemy extends GenericNPC {
     private int level;
+
+    public Enemy(String name, NPCType type, ArrayList<String> backstory) {
+        super(name, type, backstory);
+    }
 
     public int getLevel() {
         return level;
@@ -40,4 +23,4 @@ public abstract class Enemy extends NPC {
         this.level = level;
     }
 }
->>>>>>> bf7bdbb848c9f0dec3b058375a054bcfa80db36b:src/main/java/io/github/jwolff52/cyoa/entity/npc/enemy/Enemy.java
+
